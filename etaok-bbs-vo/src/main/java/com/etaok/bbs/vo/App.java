66e8583da504f83f9ba5b4e73b4dc06239d51a8e
Chales <793067761@qq.com>
2016-11-23 13:19:59 +0800

    master
    Change-Id: I9214dad55015f42656dba521ec123776417a3af8
    Signed-off-by: Chales <793067761@qq.com>

---
+++ etaok-bbs-vo/src/main/java/com/etaok/bbs/vo/App.java
@@ -0,0 +1,13 @@
+package com.etaok.bbs.vo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
