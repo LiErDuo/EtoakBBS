665db09fdd61186fed77da90d78345554cc5d6b5
Lin <732798951@qq.com>
2016-11-24 17:27:44 +0800

    最终包结构
    Signed-off-by: Lin <732798951@qq.com>

---
+++ etoak-bbs-vo/src/main/java/com/etoak/bbs/vo/App.java
@@ -0,0 +1,13 @@
+package com.etoak.bbs.vo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
